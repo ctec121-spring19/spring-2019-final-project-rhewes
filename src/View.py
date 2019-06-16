0ebc54877071eda56ee67c4a0c750f56dbacf6c5
Riley Hewes <hewesrj@gmail.com>
2019-06-16 16:56:13 -0700

    Added View

--- src/View.py
+++ src/View.py
@@ -9,6 +9,7 @@ class View:
     def __init__(self):
         self.win = GraphWin("Tic Tac Toe",300,400)
         self.win.setCoords(0, 0, 3, 4)
+        self.objList = []
         
         # Horizontal
         Line(Point(0, 1), Point(3, 1)).draw(self.win)
@@ -20,26 +21,55 @@ class View:
         Line(Point(2, 0), Point(2, 3)).draw(self.win)
     
         # Text box
-        self.topText = Text(Point(250,25),"Test")
+        self.topText = Text(Point(1.5, 3.5),"")
         self.topText.setStyle("bold")
         self.topText.setSize(18)
         self.topText.draw(self.win)
     
-    def getClick():
+    def getClick(self):
         pt = self.win.getMouse()
-
+        
         if pt.getX() <= 3 and pt.getY() <= 3:
-            cellNum = int(getX) * 3 + int(getY)
+            cellNum = int(pt.getY()) * 3 + int(pt.getX())
         else:
             cellNum = -1
         return cellNum
 
+    # Calculate coordinates from cellNum
+    def drawO(self, cellNum):
+        y = cellNum // 3
+        x = cellNum % 3
+        center = Point(x + 0.5, y + 0.5)
+        c = Circle(center, .45)
+        c.draw(self.win)
+        self.objList.append(c)
+
+    def drawX(self,cellNum):
+        # draw 2 diagonal lines
+        # add both lines to objList
+        pass
+
+    def reset(self):
+        for obj in self.objList:
+            obj.undraw()
+        self.objList.clear()
+        self.startText.setText("")
+
     def startText(self, message):
-        self.topText.setText("")
+        self.topText.setText(message)
 
 def ViewTest():
     v = View()
-    v.startText("test")
+    # test setting text
+    v.startText("Test")
+    # test getClick
+    # print(v.getClick())
+    # test draw O
+    for i in range(9):
+        v.drawO(i)
+
+    input()
+    v.reset()
     input()
     
 if __name__ == "__main__":
